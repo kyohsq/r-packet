4b81e25eac98b3d9c822c8c78428571bc31f43e4
K. Yamada <kyohsq@gmail.com>
2024-02-15 09:26:46 +0900

    update

--- packet.R
+++ packet.R
@@ -6,7 +6,8 @@ SIGNATURE1 = as.raw(c(0xD4, 0xC3, 0xB2, 0xA1))  # timestamps are in usec
 SIGNATURE2 = as.raw(c(0x4D, 0x3C, 0xB2, 0xA1))  # timestamps are in nsec
 
 L2_TYPE <- list()
-L2_TYPE[[113]] <- "Linux SLL"
+L2_TYPE[[0x0001]] <- "Ethernet"
+L2_TYPE[[0x0071]] <- "Linux SLL"
 
 L3_TYPE <- list()
 L3_TYPE[[0x0800]] <- "IPv4"
@@ -20,13 +21,6 @@ as_byte <- function(array, byte_offset){
     return(array[byte_offset + 1])
 }
 
-# as_bool(array, byte_offset = 0, bit_offset = 0) returns LSB of 1st byte
-as_bit <- function(array, byte_offset, bit_offset){
-    byte <- array[byte_offset + 1] %>% as.numeric()
-    mask <- 2 ^ bit_offset
-    return(as.numeric(bitwAnd(byte, mask) != 0))
-}
-
 # if n = -1, returns rest of the array from byte_offset
 as_byte_array <- function(array, byte_offset, n = -1){
     if(n == -1){
@@ -35,13 +29,37 @@ as_byte_array <- function(array, byte_offset, n = -1){
     return(array[byte_offset + (1:n)])
 }
 
-as_bit_array <- function(array, byte_offset, bit_offset, n){
-    return(map_dbl(bit_offset - 1 + (1:n), as_bit, array = array, byte_offset = byte_offset))
+# NOTE:
+# e.g. byte_offset = 0 byte, bit_offset = 0 bit means *MSB* of 1st byte, not LSB
+#  (x, y) means (byte_offset, bit_offset)
+#  MSB <- | 1st byte    | | 2nd byte    | | 3rd byte    | -> LSB
+#         0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1
+#         ^ (0, 0)      ^ (0, 7)              ^ (2, 2)
+#
+# bit_offset must be 0-7 and must not be > 8
+as_bit <- function(array, byte_offset, bit_offset){
+    byte <- array[byte_offset + 1] %>% as.numeric()
+    mask <- 2 ^ (7 - bit_offset)
+    return(as.numeric(bitwAnd(byte, mask) != 0))
 }
 
-as_sub_u8 <- function(array, byte_offset, bit_offset, n){
-    bit_array <- as_bit_array(array, byte_offset, bit_offset, n)
-    weight <- 2 ^ ((1:n - 1))
+# NOTE:
+# e.g. byte_offset = 1 byte, bit_offset = 2 bits, n = 14 bits means
+#  MSB <- | 1st byte    | | 2nd byte    | | 3rd byte    | -> LSB
+#         0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1
+#                             <----------HERE----------->
+as_bit_array <- function(array, byte_offset, bit_offset, n){
+    args <-
+        tibble(i = byte_offset * 8 + bit_offset + (1:n) - 1) %>%
+        mutate(byte_offset = floor(i / 8), bit_offset = i %% 8) %>%
+        select(-i)
+    return(pmap_dbl(args, as_bit, array = array))
+}
+
+# convert bit array into numeric
+weight <- function(bit_array){
+    n <- length(bit_array)
+    weight <- 2 ^ ((n:1) - 1)
     return(sum(bit_array * weight))
 }
 
@@ -80,7 +98,7 @@ as_ipv4 <- function(array){
 
 
 # body
-read_pcap <- function(path){
+read_pcap <- function(path, type = "unknown"){
     con <- file(description = path, open = "rb")
 
     # ==== parse header ====
@@ -110,18 +128,6 @@ read_pcap <- function(path){
         link_type = link_type
     )
 
-    # tentative
-    if(link_type == 0){
-        link_type <- 113  # to Linux SLL
-    }
-
-    tryCatch({
-        type <<- L2_TYPE[[link_type]]
-    },
-    error = function(e){
-        type <<- "unknown"
-    })
-
     no <- 1
     rows <- list()
     repeat{
@@ -147,6 +153,9 @@ read_pcap <- function(path){
     }
     close(con)
 
+    # DEBUG
+    # dfrm <- bind_rows(rows)
+    
     dfrm <-
         bind_rows(rows) %>%
         mutate(parsed = map(data, parse_packet, type = type))
@@ -156,15 +165,18 @@ read_pcap <- function(path){
 
 
 parse_packet <- function(array, type, recursive = T, level = 1){
-    if(type == "Linux SLL"){
+    if(type == "Ethernet"){
+        dfrm <- parse_ethernet(array, level)
+    }
+    else if(type == "Linux SLL"){
         dfrm <- parse_linux_sll(array, level)
     }
     else if(type == "IPv4"){
         dfrm <- parse_ipv4(array, level)
     }
     else{
-        if(type != "none" && type != "unknown"){
-            print(str_c("error: parser for ", type, " was not found"))
+        if(type != "none" && !str_starts(type, "unknown")){
+            print(str_c("ERROR: parser for ", type, " was not found"))
         }
         dfrm <- tibble()
         recursive = F  # turn off recursion
@@ -184,70 +196,85 @@ parse_packet <- function(array, type, recursive = T, level = 1){
 }
 
 
-parse_linux_sll <- function(array, level = 1){
-    packet_type <- as_u16_big(array, 0)
-    # arphrd <- as_u16_big(array, 2)
-    address_length <- as_u16_big(array, 4)
-    address <- as_byte_array(array, 6, address_length) %>% as_mac()
-    network_type <- as_u16_big(array, 14)
-    payload <- as_byte_array(array, 16, -1)
-
+parse_ethernet <- function(array, level = 1){
+    l3_type <- as_u16_big(array, 12)
     tryCatch({
-        type_next <<- L3_TYPE[[network_type]]
+        type_next <<- L3_TYPE[[l3_type]]
     },
     error = function(e){
-        type_next <<- "unknown"
+        print(str_c("ERROR: unknown L3 type No. (0x", as.hexmode(l3_type), ")"))
+        type_next <<- str_c("unknown (0x", as.hexmode(l3_type), ")")
+    })
+
+    return(tibble(
+        level = level,
+        type = "Ethernet",
+        info = list(tibble(
+            dst_mac = as_byte_array(array, 0, 6) %>% as_mac(),
+            src_mac = as_byte_array(array, 6, 6) %>% as_mac()
+            # type = as_u16_big(array, 12)
+        )),
+        type_next = type_next,
+        payload = as_byte_array(array, 14, -1) %>% list(),
+        raw_header = as_byte_array(array, 0, 14) %>% list()
+    ))
+}
+
+
+parse_linux_sll <- function(array, level = 1){
+    l3_type <- as_u16_big(array, 14)
+    tryCatch({
+        type_next <<- L3_TYPE[[l3_type]]
+    },
+    error = function(e){
+        print(str_c("ERROR: unknown L3 type No. (0x", as.hexmode(l3_type), ")"))
+        type_next <<- str_c("unknown (0x", as.hexmode(l3_type), ")")
     })
 
     return(tibble(
         level = level,
         type = "Linux SLL",
         info = list(tibble(
-            packet_type = packet_type,
-            address = address
+            packet_type = as_u16_big(array, 0),
+            # arphrd = as_u16_big(array, 2),
+            # address_length = as_u16_big(array, 4),
+            address = as_byte_array(array, 6, address_length) %>% as_mac()
+            # type = as_u16_big(array, 14)
         )),
         type_next = type_next,
-        payload = list(payload),
-        raw = list(array)
+        payload = as_byte_array(array, 16, -1) %>% list(),
+        raw_header = as_byte_array(array, 0, 16) %<% list()
     ))
 }
 
 parse_ipv4 <- function(array, level = 1){
-    ToS <- array[2]
-    packet_length <- as_u16_big(array, 2)
-    id <- as_u16_big(array, 4)
-    dont_fragment <- as_bit(array, 6, 6)
-    more_fragment <- as_bit(array, 6, 5)
-    fragment_offset <- as_sub_u8(array, 6, 0, 5)
-    TTL <- as_u8(array, 8)
-    protocol <- as_u8(array, 9)
-    src_ip <- as_byte_array(array, 12, 4) %>% as_ipv4()
-    dst_ip <- as_byte_array(array, 16, 4) %>% as_ipv4()
-    payload <- as_byte_array(array, 20, -1)
-
+    l4_type <- as_u8(array, 9)
     tryCatch({
-        type_next <<- L4_TYPE[[protocol]]
+        type_next <<- L4_TYPE[[l4_type]]
     },
     error = function(e){
-        type_next <<- "unknown"
+        # DEBUG: print(str_c("ERROR: unknown L4 type No. (0x", as.hexmode(l4_type), ")"))
+        type_next <<- str_c("unknown (0x", as.hexmode(l4_type), ")")
     })
 
     return(tibble(
         level = level,
         type = "IPv4",
         info = list(tibble(
-            ToS = ToS,
-            packet_length =packet_length,
-            id = id,
-            dont_fragment = dont_fragment,
-            more_fragment = more_fragment,
-            fragment_offset = fragment_offset,
-            TTL = TTL,
-            src_ip = src_ip,
-            dst_ip = dst_ip
+            version         = as_bit_array(array, 0, 0, 4) %>% weight(),
+            header_length   = as_bit_array(array, 0, 4, 4) %>% weight(),
+            ToS             = as_u8(array, 1),
+            packet_length   = as_u16_big(array, 2),
+            id              = as_u16_big(array, 4),
+            dont_fragment   = as_bit(array, 6, 1),
+            more_fragment   = as_bit(array, 6, 2),
+            fragment_offset = as_bit_array(array, 6, 3, 13) %>% weight(),
+            TTL             = as_u8(array, 8),
+            src_ip          = as_byte_array(array, 12, 4) %>% as_ipv4(),
+            dst_ip          = as_byte_array(array, 16, 4) %>% as_ipv4()
         )),
         type_next = type_next,
-        payload = list(payload),
-        raw = list(array)
+        payload = as_byte_array(array, 20, -1) %>% list(),
+        raw_header = as_byte_array(array, 0, 20) %>% list()
     ))
 }
